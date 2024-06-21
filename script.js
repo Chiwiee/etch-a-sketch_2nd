ab0c32eb4d5e498a426bbe12961c49d6cff518e0
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-06-21 09:30:09 +0800

    Add buttonDiv createelemnt

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 const body = document.querySelector("body");
 // createElement
 const mainContainer = document.createElement("div");
+const buttonDiv = document.createElement("div");
 // classList
 mainContainer.classList = "main-container";
 // textContent
+// appendChild
+body.appendChild(main);
