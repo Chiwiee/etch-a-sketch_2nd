8857d464d58c69e58a8c2cdc453e6db7c6041ddd
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-06-21 09:26:07 +0800

    Add main-container classList

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const body = document.querySelector("body");
 // createElement
 const mainContainer = document.createElement("div");
 // classList
+mainContainer.classList = "main-container";
 // textContent
