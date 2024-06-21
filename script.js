bae9f1173f5be3947c3e9d1c04f330ce3042e665
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-06-21 10:42:31 +0800

    Change name gridMain to gridContainer

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ const body = document.querySelector("body");
 const mainContainer = document.createElement("div");
 const buttonDiv = document.createElement("div");
 const button = document.createElement("button");
-const mainGrid = document.createElement("div");
+const gridContainer = document.createElement("div");
 
 // classList
 mainContainer.classList = "main-container";
 buttonDiv.classList = "button-div";
 button.classList = "button";
-mainGrid.classList = "main-grid";
+gridContainer.classList = "grid-container";
 // textContent
 button.textContent = "EDIT THE GRID SIZE";
 // appendChild
 body.appendChild(mainContainer);
 mainContainer.appendChild(buttonDiv);
 buttonDiv.appendChild(button);
-mainContainer.appendChild(mainGrid);
+mainContainer.appendChild(gridContainer);
 
 for (let i = 1; i <= 16 * 16; i++) {
-  const gridContainer = document.querySelector(".main-grid");
+  const gridContainer = document.querySelector(".grid-container");
   const grid = document.createElement("div");
   grid.classList = "grid";
   gridContainer.appendChild(grid);
